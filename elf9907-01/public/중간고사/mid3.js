c822ec11e1f0f9f37dfd6488722ab63464b15dda
Lim Subin <elf9907@gmail.com>
2020-05-04 11:00:19 +0900

    중간고사 제출

--- elf9907-01/public/중간고사/mid3.js
+++ elf9907-01/public/중간고사/mid3.js
@@ -1,11 +1,13 @@
-let persons1 = [];
+let persons1 = [];  //비어있는 persons1 배열을 생성한다.
 
-for(let i=0;i<3;i++){
+for(let i=0;i<3;i++){    //for 문에서 객체를 생성하여 persons 배열의 원소에 대입
 
     persons1[i] = {name : "홍길동", age : 16+i};
 }
 
-let persons2 = persons1;
+let persons2 = persons1;   //persons1 배열을 얕은 복사하여 persons2 배열을 생성한다
+
 persons1[0].age = 20;
+
 console.log(persons1);
 console.log(persons2);
--- elf9907-01/public/중간고사/mid5.js
+++ elf9907-01/public/중간고사/mid5.js
@@ -1,11 +1,11 @@
 let a1 = [];
 
 for(let i=0;i<100;i++){
-    a1[i] = Math.floor(Math.random()*(100-1+1) + 1);
+    a1[i] = Math.floor(Math.random()*(100-1+1) + 1);  //, 1 이상 100 이하의 정수 난수를 100개 생성
 }
 
-let a2 = a1.filter((e)=>e%2==0);
+let a2 = a1.filter((e)=>e%2==0);  //짝수 필터링
 
-let sum = a2.reduce((a,b)=>a+b);
+let sum = a2.reduce((a,b)=>a+b);  // 짝수의 합 
 
-console.log(sum);
+console.log(sum);   //합 출력
