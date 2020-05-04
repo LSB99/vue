5a5e7f73a8b9a62341f1439721eb509be2909c9b
Lim Subin <elf9907@gmail.com>
2020-05-04 11:46:06 +0900

    중간고사 제출

--- elf9907-01/public/중간고사/mid3.js
+++ elf9907-01/public/중간고사/mid3.js
@@ -1,6 +1,6 @@
 let persons1 = [];  //비어있는 persons1 배열을 생성한다.
 
-for(let i=0;i<3;i++){    //for 문에서 객체를 생성하여 persons 배열의 원소에 대입
+for(let i=0;i<3;i++){    //for 문에서 객체를 생성하여 persons1 배열의 원소에 대입
 
     persons1[i] = {name : "홍길동", age : 16+i};
 }
