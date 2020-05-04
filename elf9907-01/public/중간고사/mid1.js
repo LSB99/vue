e12998db3012b1a26a0f2313d83d9d55bfabbf2a
Lim Subin <elf9907@gmail.com>
2020-05-04 11:19:50 +0900

    중간고사 제출

--- elf9907-01/public/중간고사/mid1.js
+++ elf9907-01/public/중간고사/mid1.js
@@ -2,10 +2,10 @@ let a = [];  //배열을 만든다.
 let sum = 0;
 
 for(let i=0;i<100;i++){
-    a[i] = Math.floor(Math.random()*(100-1+1) + 1);
+    a[i] = Math.floor(Math.random()*(100-1+1) + 1);  //1 이상 100 이하의 정수 난수를 100개 생성, 배열 채움
     sum+=a[i];
 }
-
-let average = sum/100;
+ 
+let average = sum/100;   //배열의 원소의 평균
 
 console.log(average.toFixed(1));  //평균을 소수점 아래 한 자리까지 출력
--- elf9907-01/public/중간고사/mid2.js
+++ elf9907-01/public/중간고사/mid2.js
@@ -1,10 +1,11 @@
-let a = [];
+let a = [];   
 
 for(let i=0;i<100;i++){
-    a[i] = Math.floor(Math.random()*(100-1+1) + 1);
+    a[i] = Math.floor(Math.random()*(100-1+1) + 1);  //1 이상 100 이하의 정수 난수를 100개 생성, 배열 채움
 }
 
-a.sort(function (n1,n2){ 
+a.sort(function (n1,n2){   //배열을 내림차순으로 정렬
     return n2-n1; 
 });
+
 console.log(a);
