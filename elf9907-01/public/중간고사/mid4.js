012a13f582a2be486e76ccd9355d8e84b89dcd32
Lim Subin <elf9907@gmail.com>
2020-05-04 11:40:26 +0900

    중간고사 제출

--- elf9907-01/public/중간고사/mid4.js
+++ elf9907-01/public/중간고사/mid4.js
@@ -1,18 +1,18 @@
-let sum = function(array){    //sum 함수의 파라미터는 배열이다. sum 함수는 배열의 원소의 합을 리턴한다.
+function sum(array){    //sum 함수의 파라미터는 배열이다. 
     let total = 0;
 
     for(let i=0;i<array.length;i++){
         total+=array[i];
     }
-    return total;
+    return total;   //배열의 원소의 합을 리턴
 }
 
-function test_sum(f){
+function test_sum(f){   //test_sum 함수의 파라미터는 콜백 함수
 
     let a1 = [];   
 
     for(let i=0;i<5;i++){   //랜덤 값이 5개 들어있는 배열을 생성하고, 
-        a1[i] = Math.floor(Math.random()*(10-0+1) + 0); // 0~10 정수
+        a1[i] = Math.floor(Math.random()*(10-0+1) + 0); // 랜덤값 범위 0~10 정수
     }
     console.log(a1);  //그 배열을 출력하고,
 
@@ -21,7 +21,7 @@ function test_sum(f){
 }
 
 
-for(let i=0;i<5;i++){   //test_sum 함수를 5회 반복 호출하라.
-    test_sum(sum);
+for(let i=0;i<5;i++){   //test_sum 함수를 5회 반복 호출한다.
+    test_sum(sum);   //sum 함수를 파라미터로 전달
     console.log("\n");
 }
--- elf9907-01/public/중간고사/mid5.js
+++ elf9907-01/public/중간고사/mid5.js
@@ -1,7 +1,7 @@
 let a1 = [];
 
 for(let i=0;i<100;i++){
-    a1[i] = Math.floor(Math.random()*(100-1+1) + 1);  //, 1 이상 100 이하의 정수 난수를 100개 생성
+    a1[i] = Math.floor(Math.random()*(100-1+1) + 1);  //, 1 이상 100 이하의 정수 난수를 100개 생성, 배열채움
 }
 
 let a2 = a1.filter((e)=>e%2==0);  //짝수 필터링
